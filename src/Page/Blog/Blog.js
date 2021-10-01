89eb5dbc76e1a87e8c38e9f0741eebb00eff0400
saranjeet singh <grewalele@gmail.com>
2021-10-01 18:24:35 -0400

    code box work

--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -1,4 +1,5 @@
 
+
 import Sidebar from '../../component/Sidebar/Sidebar'
 import Django from '../../Content/Django/Django'
 import JavaScipt from '../../Content/JavaScript/JavaScript'
@@ -6,14 +7,30 @@ import ReactJS from '../../Content/ReactJS/ReactJS'
 import './Blog.css';
 
 
-export default function Blog(clicked) {
+export default function Blog() {
+  const thisIsMyCopy = '<body><h1>Hey it is a test page</h1><p>It is the first paragraph</p><p>It is the second paragraph</p></body>'
   return (
+    
 
     <div className="main">
       <h1>Blogs/Tutorials</h1>
       <h3>Python Tutorials</h3>
       <h3>JavaScipt Tutorials</h3>
       <h3>React Tutorials</h3>
+
+      <h2>JavaScipt in DOM</h2>
+      <p>JavaScript is the language of the web. If you ever want to feel like working with web pages, you need to learn JavaScript. In this chapter, we will learn how to use JS to manipulate the DOM ( document object model ), DOM is actually a programming interface for web documents [1].</p>
+
+      <div className="code">
+      <p>{thisIsMyCopy}</p>
+      {thisIsMyCopy}
+      </div>
+
+
+
+
+
+
     </div>
 
   )
