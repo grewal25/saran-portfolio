9c390e10a8c97ea554f14d324d4f21b1727e2939
saranjeet singh <grewalele@gmail.com>
2021-10-09 00:24:34 -0400

    layout fixes

--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -1,88 +1,84 @@
-
-
-import Sidebar from '../../component/Sidebar/Sidebar'
-import Django from '../../Content/Django/Django'
-import JavaScipt from '../../Content/JavaScript/JavaScript'
-import ReactJS from '../../Content/ReactJS/ReactJS'
-import './Blog.css';
-
+import Sidebar from "../../component/Sidebar/Sidebar";
+import Django from "../../Content/Django/Django";
+import JavaScipt from "../../Content/JavaScript/JavaScript";
+import ReactJS from "../../Content/ReactJS/ReactJS";
+import "./Blog.css";
 
 export default function Blog() {
-  const thisIsMyCopy = '<body><h1>Hey it is a test page</h1><p>It is the first paragraph</p><p>It is the second paragraph</p></body>'
+ 
+  const thisIsMyCopy =
+    "<body><h1>Hey it is a test page</h1><p>It is the first paragraph</p><p>It is the second paragraph</p></body>";
   return (
-    
-
-    <div className="main">
+    <div>
+    <div className="top-level">
+      <Sidebar />
+      <div className="main">
+       
       <h1>Blogs/Tutorials</h1>
       <h3>Python Tutorials</h3>
       <h3>JavaScipt Tutorials</h3>
       <h3>React Tutorials</h3>
 
       <h3>why smart people should vote</h3>
-      <p>Generally in a democratic country, voting turns out to remain in 50-60% of the range. In some cases, it can go upto 70-80% but that’s very rare. Now, that makes me very curious. Who doesn’t vote and why?
+      <p>
+        Generally in a democratic country, voting turns out to remain in 50-60%
+        of the range. In some cases, it can go upto 70-80% but that’s very rare.
+        Now, that makes me very curious. Who doesn’t vote and why? I think one
+        of the reasons people vote is to make their candidate win and majority
+        of people who don’t vote are those who either can’t vote and a small
+        minority chooses not to vote. And if you ask them why they don’t vote,
+        they may reply
+      </p>
 
-I think one of the reasons people vote is to make their candidate win and majority of people who don’t vote are those who either can’t vote and a small minority chooses not to vote. And if you ask them why they don’t vote, they may reply
-</p>
+      <ul>
+        <li>Candidates are not good</li>
+        <li>
+          <li>Voting doesn’t change anything</li>
+        </li>
+        <li>Have no time for politics</li>
+        <li>Believe in totalitarianism</li>
+      </ul>
 
-<ul>
-  <li>Candidates are not good
-</li>
-<li><li>Voting doesn’t change anything
-</li>
-</li>
-<li>Have no time for politics
-</li>
-<li>Believe in totalitarianism 
-</li>
-</ul>
+      <p>
+        50% of voting turn over is scary. It means less and less people are
+        believing in the democratic systems and people living in western
+        democracies have no idea what it looks like to live in a “non-democratic
+        country”. More and more people opting out of voting will cause people to
+        reduce trust in the democracy and nobody wants to see how a
+        non-democratic country works nor they want to live in that country.
+      </p>
 
-<p>50% of voting turn over is scary. It means less and less people are believing in the democratic systems and people living in western democracies have no idea what it looks like to live in a “non-democratic country”.
-More and more people opting out of voting will cause people to reduce trust in the democracy and nobody wants to see how a non-democratic country works nor they want to live in that country. 
-</p>
-
-<h4>So, how can we improve that?</h4>
+      <h4>So, how can we improve that?</h4>
 
       <h2>JavaScipt in DOM</h2>
-      <p>JavaScript is the language of the web. If you ever want to feel like working with web pages, you need to learn JavaScript. In this chapter, we will learn how to use JS to manipulate the DOM ( document object model ), DOM is actually a programming interface for web documents [1].</p>
+      <p>
+        JavaScript is the language of the web. If you ever want to feel like
+        working with web pages, you need to learn JavaScript. In this chapter,
+        we will learn how to use JS to manipulate the DOM ( document object
+        model ), DOM is actually a programming interface for web documents [1].
+      </p>
 
       <div className="code">
-      <p>{thisIsMyCopy}</p>
-      {thisIsMyCopy}
+        <p>{thisIsMyCopy}</p>
+        {thisIsMyCopy}
       </div>
-
-
-
-
-
-
+      </div>
+      </div>
     </div>
-
-  )
-
+  );
 
   // if (!clicked) {
   //   return <Django />;
   // }
   // return <ReactJS />;
-
-
-
-
 }
-
-
-
-
-
-
-
 
 // function Blog(clicked) {
 //   console.log("from Blog",clicked.)
 //   return(
 //      <div>
 //       <Sidebar  />
-//       { clicked ? 
+//       { clicked ?
 //         <Django  />
 //       :
 //       <ReactJS />
@@ -92,10 +88,6 @@ More and more people opting out of voting will cause people to reduce trust in t
 
 //   );
 // }
-
-
-
-
 
 // render() {
 //   const isLoggedIn = this.state.isLoggedIn;
@@ -108,10 +100,6 @@ More and more people opting out of voting will cause people to reduce trust in t
 //     </div>
 //   );
 // }
-
-
-
-
 
 // if(clicked){
 //   return <Django />
--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -1,34 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
+import "./Sidebar.css";
 
+const Sidebar = () => {
+  const [clicked, setClicked] = useState(true);
 
-
-const Sidebar = ()=>{
-    const [clicked, setClicked] = useState(true)
-    // const handleClick=(e)=>{
-    //      e.preventDefault
-    //      console.log('The link was clicked.');
-    // }
-    console.log("from sidebar",clicked)
-    return(
-        <div className="sidebar-title">
-        <button onClick={
-            ()=>setClicked(false)
-        }>React Blog </button>
-        <button
-            onClick={
-                ()=>setClicked(false)}
-        >Django</button>
-        {/* <button
-            onClick={
-                ()=>setClicked(true)}
-        >Python Blog</button>
-        <button
-            onClick={
-                ()=>setClicked(true)}
-        >Django Blog</button> */}
+  console.log("from sidebar", clicked);
+  return (
+    <div className=" sidebar-title">
         
-        </div>
-    )
-}
 
-export default Sidebar
+      <div className="button react-blog">
+        <button onClick={() => setClicked(false)}>JavaScipt </button>
+      </div>
+      <div className="django-blog">
+        <button onClick={() => setClicked(false)}>React</button>
+      </div>
+      <div className="react-blog">
+        <button onClick={() => setClicked(false)}>Python </button>
+      </div>
+      <div className="django-blog">
+        <button onClick={() => setClicked(false)}>Django</button>
+      </div>
+      <div className="react-blog">
+        <button onClick={() => setClicked(false)}>Functional Programming</button>
+      </div>
+      <div className="django-blog">
+        <button onClick={() => setClicked(false)}>OOP</button>
+      </div>
+      <div className="react-blog">
+        <button onClick={() => setClicked(false)}>Algorithms </button>
+      </div>
+     
+    </div>
+  );
+};
+
+export default Sidebar;
