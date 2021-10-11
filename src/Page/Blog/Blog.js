e2d336153f6427b2c453271a052f636b4eda2a1b
saranjeet singh <grewalele@gmail.com>
2021-10-11 19:51:23 -0400

    added new essay component

--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -5,51 +5,30 @@ import ReactJS from "../../Content/ReactJS/ReactJS";
 import "./Blog.css";
 
 export default function Blog() {
- 
+
   const thisIsMyCopy =
     "<body><h1>Hey it is a test page</h1><p>It is the first paragraph</p><p>It is the second paragraph</p></body>";
   return (
     <div>
     <div className="top-level">
-      <div className="side-bar"><Sidebar /></div>
+      <div className="side-bar"><Sidebar />
+     
+      </div>
       
-      <div className="main">
+      <div className="blog-main">
+
+      </div>
+      </div>
+    </div>
+  );
+
        
-      <h1>Blogs/Tutorials</h1>
+      {/* <h1>Blogs/Tutorials</h1>
       <h3>Python Tutorials</h3>
       <h3>JavaScipt Tutorials</h3>
       <h3>React Tutorials</h3>
 
-      <h3>why smart people should vote</h3>
-      <p>
-        Generally in a democratic country, voting turns out to remain in 50-60%
-        of the range. In some cases, it can go upto 70-80% but that’s very rare.
-        Now, that makes me very curious. Who doesn’t vote and why? I think one
-        of the reasons people vote is to make their candidate win and majority
-        of people who don’t vote are those who either can’t vote and a small
-        minority chooses not to vote. And if you ask them why they don’t vote,
-        they may reply
-      </p>
-
-      <ul>
-        <li>Candidates are not good</li>
-        <li>
-          <li>Voting doesn’t change anything</li>
-        </li>
-        <li>Have no time for politics</li>
-        <li>Believe in totalitarianism</li>
-      </ul>
-
-      <p>
-        50% of voting turn over is scary. It means less and less people are
-        believing in the democratic systems and people living in western
-        democracies have no idea what it looks like to live in a “non-democratic
-        country”. More and more people opting out of voting will cause people to
-        reduce trust in the democracy and nobody wants to see how a
-        non-democratic country works nor they want to live in that country.
-      </p>
-
-      <h4>So, how can we improve that?</h4>
+      
 
       <h2>JavaScipt in DOM</h2>
       <p>
@@ -62,11 +41,7 @@ export default function Blog() {
       <div className="code">
         <p>{thisIsMyCopy}</p>
         {thisIsMyCopy}
-      </div>
-      </div>
-      </div>
-    </div>
-  );
+      </div> */}
 
   // if (!clicked) {
   //   return <Django />;
--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -1,27 +1,45 @@
 import { useState } from "react";
+import Django from "../../Content/Django/Django";
+import Essays from "../../Content/Essays/Essays";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  const [clicked, setClicked] = useState(true);
+  const [javaScript, setJavaScript] = useState(false);
+  const [react, setReact] = useState(false);
+  const [python, setPython] = useState(false);
+  const [django, setDjango] = useState(false);
+  const [essays, setEssays] = useState(false);
 
-  console.log("from sidebar", clicked);
+  console.log("from sidebar", javaScript);
   return (
     <div className=" sidebar-title">
         
 
       <div className="button react-blog">
-        <button onClick={() => setClicked(false)}>JavaScipt </button>
+        <button onClick={() => setJavaScript(!javaScript)}>JavaScipt </button>
       </div>
+
+     {javaScript && <Django />}
+
+     <div className="button react-blog">
+        <button onClick={() => setEssays(!essays)}>Essays</button>
+        {essays && <Essays />}
+      </div>
+     
       <div className="django-blog">
-        <button onClick={() => setClicked(false)}>React</button>
+        <button onClick={() => setReact(true)}>React</button>
       </div>
       <div className="react-blog">
-        <button onClick={() => setClicked(false)}>Python </button>
+        <button onClick={() => setPython(true)}>Python </button>
       </div>
       <div className="django-blog">
-        <button onClick={() => setClicked(false)}>Django</button>
+        <button onClick={() => setDjango(true)}>Django</button>
       </div>
-      <div className="react-blog">
+
+
+
+
+      {/* <div className="react-blog">
         <button onClick={() => setClicked(false)}>Functional Programming</button>
       </div>
       <div className="django-blog">
@@ -29,7 +47,7 @@ const Sidebar = () => {
       </div>
       <div className="react-blog">
         <button onClick={() => setClicked(false)}>Algorithms </button>
-      </div>
+      </div> */}
      
     </div>
   );
