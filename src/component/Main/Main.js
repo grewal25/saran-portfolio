18b11e69719cdbf1c3386a83a5fa69c6b56b298d
saranjeet singh <grewalele@gmail.com>
2021-10-17 20:19:49 -0400

    layout fixes

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -4,22 +4,41 @@ import "./Main.css";
 
 export default function Main() {
   return (
-    <div className="top-main">
-      <div className="top-info"><TopInfo /></div>
-      <div className="main-bottom">
-        <h1>ABOUT ME</h1>
-        <p>
-          I am a Web Developer currently living in Toronto, Canada. Past 4 years
-          I have been playing with many web technologies. From Front-end
-          framework ReactJS, Angular to Backend Django and Flask. I have a
-          Masters degree in Electrical Engineering from University of British
-          Columbia. Currently I am working as Front-End Developer in Bank of
-          Montreal and on the side am involved in number of projects building
-          websites for small and medium sized businesses. You can see my
-          projects
-          
-        </p>
+    <>
+      <div className="top-main">
+        <div className="top-info">
+          <TopInfo />
+        </div>
+        <div className="main-bottom">
+          <h3>ABOUT ME</h3>
+          <p>
+            I am a Web Developer currently living in Toronto, Canada. Past 4
+            years I have been playing with many web technologies. From Front-end
+            framework ReactJS, Angular to Backend Django and Flask. I have a
+            Masters degree in Electrical Engineering from University of British
+            Columbia. Currently I am working as Front-End Developer in Bank of
+            Montreal and on the side am involved in number of projects building
+            websites for small and medium sized businesses.
+          </p>
+          <div className="mid-main">
+            <h3>Most Read Tutorials</h3>
+            <ul>
+               <a href="" ></a>
+              <li><a href="" >How to write functional components in React</a></li>
+              <li><a href="" >Fundamentals of Redux</a></li>
+              <li><a href="" >Hooks in React</a></li>
+              <li><a href="" >What are Arrow functions?</a></li>
+              <li><a href="" >Django and Django Rest Framework</a></li>
+
+            </ul>
+          </div>
+
+          <div className="lower-mid">
+            <div className="lm-1"><button href="" >Read More Tutorials</button></div>
+            <div className="lm-2"><button href="" >See List of All My Projects</button></div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/component/TopInfo/TopInfo.js
+++ src/component/TopInfo/TopInfo.js
@@ -4,10 +4,12 @@ import './TopInfo.css'
 export default function TopInfo(){
     return(
        <div className="topInfo" >
-             <div className="top-img"> <img src="/flag.jpg"  alt = "my info" /></div>
+             <div className="top-img">
+                  {/* <img src="/flag.jpg"  alt = "my info" /> */}
+                  </div>
            <div className="name">
                 <h5>Saranjeet Singh</h5> 
-                <h4>Web Developer in Toronto</h4>
+                <h4>Web Developer </h4>
                  
            </div>
          
