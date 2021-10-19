ef38b53e5fd0d04633817d7a0f3d93fc517da258
saranjeet singh <grewalele@gmail.com>
2021-10-19 17:12:02 -0400

    md feature added

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
            
           </Route>
         
-        </Switch>
+  </Switch>
     </BrowserRouter>
     
 );
--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -1,4 +1,7 @@
 
+
+import Markdown from 'markdown-to-jsx';
+import { useEffect, useState } from 'react';
 import Sidebar from '../../component/Sidebar/Sidebar'
 import Django from '../../Content/Django/Django'
 import JavaScipt from '../../Content/JavaScript/JavaScript'
@@ -6,14 +9,31 @@ import ReactJS from '../../Content/ReactJS/ReactJS'
 import './Blog.css';
 
 
-export default function Blog(clicked) {
+export default function Blog() {
+  const file_name = 'test.md'
+  const [post, setPost] = useState('')
+
+  useEffect(()=>{
+    import(`/Users/saranjeetsingh/Desktop/saran-portfolio/Saranjeet-Portfolio-Website/src/markdown/${file_name}`)
+    .then(res => {
+      fetch(res.default)
+        .then(res => res.text())
+      .then(res => setPost(res));
+    })
+    .catch(err =>console.log(err))
+  })
   return (
 
+
     <div className="main">
+      <Markdown >{post}</Markdown>
+     
       <h1>Blogs/Tutorials</h1>
       <h3>Python Tutorials</h3>
       <h3>JavaScipt Tutorials</h3>
       <h3>React Tutorials</h3>
+
+
     </div>
 
   )
