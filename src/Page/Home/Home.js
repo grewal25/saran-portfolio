4cc802300c8218d2ad1bf30fe41b487dea758927
saranjeet singh <grewalele@gmail.com>
2021-11-14 11:55:15 -0500

    add tailwind class

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,17 +1,13 @@
-
-
-
+import "./Home.css";
 function Home() {
   return (
-
-    <div className="App">
-     
-     
+    <div>
+      <div className="flex border-4 border-blue-500 justify-evenly">
+        <div className="md:flex-3 lg:flex-4">hey this is the sample text.</div>
+      </div>
+      
     </div>
-  
-
-
-);
+  );
 }
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    flex: {
+      '3': '0 0 500px',
+      '4': '0 0 700px',
+    },
   },
   variants: {
     extend: {},
