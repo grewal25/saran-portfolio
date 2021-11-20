49deeecd568cf3cb5969d132da57abac5dbb001c
saranjeet singh <grewalele@gmail.com>
2021-11-20 12:40:45 -0500

    img added

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,12 +1,35 @@
 import "./Home.css";
+
 function Home() {
   return (
     <div>
-      <div className="flex border-4 border-blue-500 justify-evenly">
-        <div className="md:flex-3 lg:flex-4">
-          <div></div>
+      <div className="flex justify-evenly pt-12">
+      <div className="border-2 border-black">
+          <div>
+            <h1 className="text-3xl py-4">Hey there...</h1>
+            <p>I am Saranjeet, web developer based in Canada.</p>
+            <p>On this website, I share my knowledge, experience and tips
+              that may help you accelrate your skills especially in front-end domain.
+            </p>
+          </div>
         </div>
+        <div className="border-2 border-black">
+          <div className="w-96 h-48" ></div>
+        </div>
+
       </div>
+
+      <div className="flex justify-evenly pt-12">
+            <div className="w-48 h-48 border-2 border-black" >1</div>
+            <div className="w-48 h-48 border-2 border-black">2</div>
+            <div className="w-48 h-48 border-2 border-black">3</div>
+        </div>
+
+        <div className="flex justify-evenly pt-12">
+            <div className="w-48 h-48 border-2 border-black" >explore</div>
+            <div className="w-48 h-48 border-2 border-black">latest articles</div>
+            
+        </div>
       
     </div>
   );
