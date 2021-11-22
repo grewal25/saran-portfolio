637568a5332b12a8a6c1e7502cbb12cf31f10049
saranjeet singh <grewalele@gmail.com>
2021-11-21 20:04:36 -0500

    add svgs

--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -1,11 +1,50 @@
-
 import "./Blog.css";
 
 export default function Blog() {
-  return(
-  <div className="p-6">
-  <h1>Hello</h1>
-  </div>
-  )
-  
+  return (
+    <div >
+      <div className="flex flex-col items-center pb-12">
+        
+          <img src="/penguin.svg" />
+          <p className="font-bold text-4xl">Here is the list of all tech and non-tech blogs </p>
+        
+      </div>
+
+    <div className="flex justify-evenly px-24 gap-x-32">
+      
+      <div>
+        <p className="font-bold text-2xl">Productivity</p>
+        <div className="border-b-2 border-gray-200 py-3">how to type faste: 8 actionable tips
+        to increase your typing speed</div>
+        <div className="border-b-2 border-gray-200 py-3">how to stay focused</div>
+        <div className="border-b-2 border-gray-200 py-3">time management skills</div>
+      </div>
+      <div>
+        <p className="font-bold text-2xl">History</p>
+        <div className="border-b-2 border-gray-200 py-3">how to type faste: 8 actionable tips
+        to increase your typing speed</div>
+        <div className="border-b-2 border-gray-200 py-3">how to stay focused</div>
+        <div className="border-b-2 border-gray-200 py-3">time management skills</div>
+      </div>
+    </div>
+
+    <div className="flex justify-evenly px-24 gap-x-32 pt-12">
+      <div>
+        <p className="font-bold text-2xl">Productivity</p>
+        <div className="border-b-2 border-gray-200 py-3">how to type faste: 8 actionable tips
+        to increase your typing speed</div>
+        <div className="border-b-2 border-gray-200 py-3">how to stay focused</div>
+        <div className="border-b-2 border-gray-200 py-3">time management skills</div>
+      </div>
+      <div>
+        <p className="font-bold text-2xl">History</p>
+        <div className="border-b-2 border-gray-200 py-3">how to type faste: 8 actionable tips
+        to increase your typing speed</div>
+        <div className="border-b-2 border-gray-200 py-3">how to stay focused</div>
+        <div className="border-b-2 border-gray-200 py-3">time management skills</div>
+      </div>
+    </div>
+
+    </div>
+  );
 }
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -8,7 +8,10 @@ function Home() {
       <div className="px-6 md:flex justify-evenly pt-12 md:px-6 xl:px-64">
         <div className="border-0 border-black">
           <div>
-            <h1 className="text-3xl sm:py-2 md:py-4">Hey there...</h1>
+            <div className="flex text-3xl sm:py-2 md:py-4">
+              Hey there...
+              <img src="/penguinwave.svg" style={{ height: 40, width: 45 }}/>
+            </div>
             <p>I am Saranjeet, <span className="text-blue-400 italic font-bold">web developer </span> based in Canada.</p>
             <p className="pb-6 md:pb-0">
               On this website, I share my knowledge, experience and tips that
@@ -17,24 +20,30 @@ function Home() {
           </div>
         </div>
         <div className="border-2 border-black">
-          <div className="w-96 h-48"></div>
+          <div className="">
+            <img src="/photo.jpeg" />
+          </div>
         </div>
       </div>
 
       <div className="md:flex justify-evenly pb-12 sm:pt-12 md:pt-24 md:px-6 lg:px-6 xl:px-64 xl:mx-0 md:gap-x-8 xl:gap-x-16 2xl:gap-x-32">
-        <div className="m-6 md:m-0 pt-0 md:pt-0 border-4 border-light-blue-500 border-opacity-100 rounded-xl md:border-2 md:border-gray-100 md:rounded-xl text-center md:shadow-2xl mb-4">
-          <p className="pt-8 md:pt-8 ">React</p>
+        <div className="md:bg-white m-6 md:m-0 pt-0 md:pt-0 border-4 border-light-blue-500 border-opacity-100 rounded-xl md:border-2 md:border-gray-100 md:rounded-xl text-center md:shadow-2xl mb-4">
+          <div className="flex pt-8 md:pt-8 ">
+          <div><img src="/rocket.svg" style={{ height: 35, width: 45 }}/></div>
+            <div> React</div>
+            
+            </div>
           <p className="py-8 ">
             Find tutorials on React, React Hooks and many more new techniques
           </p>
         </div>
-        <div className="m-6 md:m-0 pt-0 md:pt-0 border-4 border-light-blue-500 border-opacity-100 rounded-xl md:border-2 md:border-gray-100 md:rounded-xl text-center md:shadow-2xl mb-4">
+        <div className="bg-white m-6 md:m-0 pt-0 md:pt-0 border-4 border-light-blue-500 border-opacity-100 rounded-xl md:border-2 md:border-gray-100 md:rounded-xl text-center md:shadow-2xl mb-4">
           <p className="pt-8">CSS</p>
           <p className="pt-8">
             Find tutorials on React, React Hooks and many more new techniques
           </p>
         </div>
-        <div className="m-6 md:m-0 pt-0 md:pt-0 border-4 border-light-blue-500 border-opacity-100 rounded-xl md:border-2 md:border-gray-100 md:rounded-xl text-center md:shadow-2xl mb-4">
+        <div className="bg-white m-6 md:m-0 pt-0 md:pt-0 border-4 border-light-blue-500 border-opacity-100 rounded-xl md:border-2 md:border-gray-100 md:rounded-xl text-center md:shadow-2xl mb-4">
           <p className="pt-8">JavaScript</p>
           <p className="py-8">
             Find tutorials on React, React Hooks and many more new techniques
@@ -73,7 +82,7 @@ function Home() {
         
           </div>
         </div>
-        <div className=" border-0 border-black">
+        <div className=" border-0 border-black pt-6 md:pt-0">
           <p className="text-3xl"><span className="border-b-4 border-blue-500">Explore</span></p>
           <div className="">
             <div className="mt-4 pb-4 border-2 border-light-blue-100 border-opacity-100 rounded-md md:bg-gray-100">
