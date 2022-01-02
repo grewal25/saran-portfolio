c96e212f68371ea9a26293e779b9f483fab9fa81
saranjeet singh <grewalele@gmail.com>
2022-01-02 18:57:22 -0500

    add new essays

--- src/Page/Archive/Archive.js
+++ src/Page/Archive/Archive.js
@@ -16,7 +16,7 @@ const Archive = () => {
                       Method to super-boost productivity
                     </div>
                     <ul>
-                      <li>
+                      <li className="font-light">
                         We all have wardrobes full of clothes and still, we
                         spend most of our time deciding what to wear. One of the
                         ways to solve this is to group all the
@@ -43,7 +43,7 @@ const Archive = () => {
                     Why you shouldn't fear the 30s
                     </div>
                     <ul>
-                      <li>
+                      <li className="font-light">
                         We all have wardrobes full of clothes and still, we
                         spend most of our time deciding what to wear. One of the
                         ways to solve this is to group all the
@@ -70,7 +70,7 @@ const Archive = () => {
                     Science of Deep Focus
                     </div>
                     <ul>
-                      <li>
+                      <li className="font-light">
                         We all have wardrobes full of clothes and still, we
                         spend most of our time deciding what to wear. One of the
                         ways to solve this is to group all the
@@ -86,7 +86,173 @@ const Archive = () => {
                 </div>
               </article>
             </div>
-          </div>
+
+            <div className="flex justify-center gap-y-12 lg:gap-x-12 pt-24 flex-col lg:flex-row items-center">
+              <article className="">
+                <div className="max-w-xs md:max-w-md lg:max-w-xs rounded-2xl overflow-hidden shadow-lg hover:shadow-3xl">
+                  <img
+                    src="prod-1.jpeg"
+                    className="w-full h-2/3 hover:shadow-3xl"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-2xl mb-2">
+                      Method to super-boost productivity
+                    </div>
+                    <ul>
+                      <li className="font-light">
+                        We all have wardrobes full of clothes and still, we
+                        spend most of our time deciding what to wear. One of the
+                        ways to solve this is to group all the
+                      </li>
+                    </ul>
+
+                    <div>
+                      <span className="inline-block bg-gray-200 rounded-full px-3 text-sm mr-2 mt-2">
+                        #productivity
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+
+              <article className="">
+                <div className="max-w-xs md:max-w-md lg:max-w-xs rounded-2xl overflow-hidden shadow-lg hover:shadow-3xl">
+                  <img
+                    src="fear.jpeg"
+                    className="w-full h-2/3 hover:shadow-3xl"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-2xl mb-2">
+                    Why you shouldn't fear the 30s
+                    </div>
+                    <ul>
+                      <li className="font-light">
+                        We all have wardrobes full of clothes and still, we
+                        spend most of our time deciding what to wear. One of the
+                        ways to solve this is to group all the
+                      </li>
+                    </ul>
+
+                    <div>
+                      <span className="inline-block bg-gray-200 rounded-full px-3 text-sm mr-2 mt-2">
+                        #philosophy
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+
+              <article className="">
+                <div className="max-w-xs md:max-w-md lg:max-w-xs rounded-2xl overflow-hidden shadow-lg hover:shadow-3xl">
+                  <img
+                    src="prod-1.jpeg"
+                    className="w-full h-2/3 hover:shadow-3xl"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-2xl mb-10">
+                    Science of Deep Focus
+                    </div>
+                    <ul>
+                      <li className="font-light">
+                        We all have wardrobes full of clothes and still, we
+                        spend most of our time deciding what to wear. One of the
+                        ways to solve this is to group all the
+                      </li>
+                    </ul>
+
+                    <div>
+                      <span className="inline-block bg-gray-200 rounded-full px-3 text-sm mr-2 mt-2">
+                        #productivity
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+
+            <div className="flex justify-center gap-y-12 lg:gap-x-12 pt-24 flex-col lg:flex-row items-center">
+              <article className="">
+                <div className="max-w-xs md:max-w-md lg:max-w-xs rounded-2xl overflow-hidden shadow-lg hover:shadow-3xl">
+                  <img
+                    src="prod-1.jpeg"
+                    className="w-full h-2/3 hover:shadow-3xl"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-2xl mb-2">
+                      Method to super-boost productivity
+                    </div>
+                    <ul>
+                      <li className="font-light">
+                        We all have wardrobes full of clothes and still, we
+                        spend most of our time deciding what to wear. One of the
+                        ways to solve this is to group all the
+                      </li>
+                    </ul>
+
+                    <div>
+                      <span className="inline-block bg-gray-200 rounded-full px-3 text-sm mr-2 mt-2">
+                        #productivity
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+
+              <article className="">
+                <div className="max-w-xs md:max-w-md lg:max-w-xs rounded-2xl overflow-hidden shadow-lg hover:shadow-3xl">
+                  <img
+                    src="fear.jpeg"
+                    className="w-full h-2/3 hover:shadow-3xl"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-2xl mb-2">
+                    Why you shouldn't fear the 30s
+                    </div>
+                    <ul>
+                      <li className="font-light">
+                        We all have wardrobes full of clothes and still, we
+                        spend most of our time deciding what to wear. One of the
+                        ways to solve this is to group all the
+                      </li>
+                    </ul>
+
+                    <div>
+                      <span className="inline-block bg-gray-200 rounded-full px-3 text-sm mr-2 mt-2">
+                        #philosophy
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+
+              <article className="">
+                <div className="max-w-xs md:max-w-md lg:max-w-xs rounded-2xl overflow-hidden shadow-lg hover:shadow-3xl">
+                  <img
+                    src="prod-1.jpeg"
+                    className="w-full h-2/3 hover:shadow-3xl"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-2xl mb-10">
+                    Science of Deep Focus
+                    </div>
+                    <ul>
+                      <li className="font-light">
+                        We all have wardrobes full of clothes and still, we
+                        spend most of our time deciding what to wear. One of the
+                        ways to solve this is to group all the
+                      </li>
+                    </ul>
+
+                    <div>
+                      <span className="inline-block bg-gray-200 rounded-full px-3 text-sm mr-2 mt-2">
+                        #productivity
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>          </div>
         </div>
       </div>
     </>
