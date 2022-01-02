dad3ac78d7866b3faadc6e888d6e323a95612544
saranjeet singh <grewalele@gmail.com>
2022-01-02 18:23:44 -0500

    add layout fixes

--- src/Page/Archive/Archive.js
+++ src/Page/Archive/Archive.js
@@ -7,9 +7,9 @@ const Archive =()=>{
             <div >
             <div>
           
-        <div className="flex justify-center gap-x-12 pt-24 ">
+        <div className="flex justify-center gap-y-12 lg:gap-x-12 pt-24 flex-col lg:flex-row items-center">
           <article className="">
-              <div className="max-w-xs rounded overflow-hidden shadow-lg hover:shadow-3xl">
+              <div className="max-w-xs md:max-w-md lg:max-w-xs rounded overflow-hidden shadow-lg hover:shadow-3xl">
                 <img src="img-1.jpeg" className="w-full h-2/3 hover:shadow-3xl"  />
                 <div className="px-6 py-4">
                     <div className="font-bold text-2xl mb-2">
@@ -40,8 +40,8 @@ const Archive =()=>{
           </article>
 
           <article className="">
-              <div className="max-w-xs rounded overflow-hidden shadow-lg">
-                <img src="img-1.jpeg" className="w-full"  />
+              <div className="max-w-xs md:max-w-md lg:max-w-xs rounded overflow-hidden shadow-lg hover:shadow-3xl">
+                <img src="img-1.jpeg" className="w-full h-2/3 hover:shadow-3xl"  />
                 <div className="px-6 py-4">
                     <div className="font-bold text-2xl mb-2">
                         Example Photo
@@ -71,8 +71,8 @@ const Archive =()=>{
           </article>
 
           <article className="">
-              <div className="max-w-xs rounded overflow-hidden shadow-lg">
-                <img src="img-1.jpeg" className="w-full"  />
+              <div className="max-w-xs md:max-w-md lg:max-w-xs rounded overflow-hidden shadow-lg hover:shadow-3xl">
+                <img src="img-1.jpeg" className="w-full h-2/3 hover:shadow-3xl"  />
                 <div className="px-6 py-4">
                     <div className="font-bold text-2xl mb-2">
                         Example Photo
@@ -102,51 +102,6 @@ const Archive =()=>{
           </article>
           </div>
 
-          <div className="hidden md:block lg:hidden max-w-7xl m-auto ">
-            <div className="flex flex-col items-center gap-y-12 px-6">
-            <div className="w-96 h-72 border-2  rounded-3xl bg-black hover:bg-yellow-300 text-white hover:text-black">
-                <a href="" className="bg-indigo-500">
-                  <div className="flex flex-col items-center justify-center h-full">
-                    <div>
-                      <h1 className="text-6xl">Development</h1>
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="w-96 h-72 border-2  rounded-3xl bg-black hover:bg-yellow-300 text-white hover:text-black">
-                <a href="" className="bg-indigo-500">
-                  <div className="flex flex-col items-center justify-center h-full">
-                    <div>
-                      <h1 className="text-6xl">Technology</h1>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="block md:hidden lg:hidden max-w-7xl m-auto ">
-            <div className="flex flex-col items-center gap-y-3 px-6">
-            <div className="w-72 h-48 border-2  rounded-3xl bg-black hover:bg-yellow-300 text-white hover:text-black">
-                <a href="" className="bg-indigo-500">
-                  <div className="flex flex-col items-center justify-center h-full">
-                    <div>
-                      <h1 className="text-4xl">Development</h1>
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="w-72 h-48 border-2 rounded-3xl bg-black hover:bg-yellow-300 text-white hover:text-black">
-                <a href="" className="bg-indigo-500">
-                  <div className="flex flex-col items-center justify-center h-full">
-                    <div>
-                      <h1 className="text-4xl">Writing</h1>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
       </div>
                 
             </div>
